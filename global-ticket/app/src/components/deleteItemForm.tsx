b15867b7dacab448afc627462bfa4e3c523e7417
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-04-18 14:05:59 +0200

    feat: implement deleteButton

---
+++ global-ticket/app/src/components/deleteItemForm.tsx
@@ -0,0 +1,12 @@
+import { deleteItem } from "../lib/actions";
+
+export default function DeleteButton({ id }: { id: number }) {
+  const deleteItemById = deleteItem.bind(null, id);
+  return (
+    <form action={() => deleteItemById()}>
+      <button className="p-2 bg-red-800 font-bold hover:bg-red-950">
+        Delete
+      </button>
+    </form>
+  );
+}
