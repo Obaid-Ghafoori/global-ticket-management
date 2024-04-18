40b96caed098ef82964c4b244a4aef9cf30741bd
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-04-18 13:25:30 +0200

    feat: add crud backend functionality

---
+++ global-ticket/app/src/lib/util.ts
@@ -0,0 +1,18 @@
+import{promises as fs} from 'fs';
+export type HolidayDestinationItem = {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export async function getHolidayDestinationsData() {
+    try {
+        const dataFile = await fs.readFile(process.cwd() + './app/lib/mockData.json', 'utf8');
+        const data = JSON.parse(dataFile);
+        return data;
+    } catch (error) {
+        console.error('Error occurred:', error);
+        throw new Error('Failed to retrieve holiday destinations\' data');
+    }
+}
+
