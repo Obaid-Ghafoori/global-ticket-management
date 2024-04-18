45c1937b45e4a2c63d2c13c115ad116e0635ca75
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-04-18 13:47:36 +0200

    feat: implement navbar

---
+++ global-ticket/app/src/components/navBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+import globoLogo from "@/public/globoticket-horizontal-white.svg";
+
+export default function NavBar() {
+  return (
+    <nav className="flex justify-between items-center mt-5">
+      <Link href={"/"}>
+        <Image
+          src={globoLogo}
+          alt="Global tickets logo"
+          width={300}
+          height={100}
+        />
+      </Link>
+      <Link className="bg-slate-700 font-bold p-2 hover:bg-slate-500" href={'/addItem'}>Add Item</Link>
+    </nav>
+  );
+}
